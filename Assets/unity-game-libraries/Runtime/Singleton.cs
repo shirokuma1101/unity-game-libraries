9be0c729c09b477908a2f2ba74b9c58b81a2264d
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2024-05-27 00:03:30 +0900

    add: Singleton.csを追加

---
+++ Assets/unity-game-libraries/Runtime/Singleton.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+
+namespace UGL.Singleton
+{
+    /// <summary>
+    /// Singleton class for MonoBehaviour.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Singleton<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance { get; private set; }
+
+
+        public virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Singleton class for MonoBehaviour that is persistent.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class SingletonPersistent<T> : MonoBehaviour where T : Component
+    {
+        public static T Instance { get; private set; }
+
+
+        public virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this as T;
+                DontDestroyOnLoad(this);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Used when Singleton (or similar classes) cannot be inherited (e.g. inherited).
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// class BaseClass : MonoBehaviour { }
+    /// class DerivedClass : BaseClass
+    /// {
+    ///     public static DerivedClass Instance
+    ///         => SingletonAttacher<DerivedClass>.Instantce;
+    /// }
+    /// </code>
+    /// </example>
+    /// <typeparam name="T"></typeparam>
+    public static class SingletonAttacher<T> where T : Component
+    {
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = Object.FindAnyObjectByType<T>();
+                }
+                return instance;
+            }
+        }
+
+        private static T instance;
+    }
+}

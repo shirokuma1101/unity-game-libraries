a3f014a805e8e1cc4f89be86bd95e1304b5b1ab8
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2024-05-27 23:24:30 +0900

    refactor: Easing.csをリファクタリング

--- Assets/unity-game-libraries/Runtime/Easing.cs
+++ Assets/unity-game-libraries/Runtime/Easing.cs
@@ -7,6 +7,7 @@ namespace UGL.Easing
     {
         public enum EasingType
         {
+            Linear,
             SineIn,
             SineOut,
             SineInOut,
@@ -42,6 +43,7 @@ namespace UGL.Easing
             __INVALID__ = -1,
         }
 
+        public static float Linear(float n) => Normalize(n, 0.0f, 1.0f);
         public static class Sine
         {
             public static float In(float n) => 1.0f - Mathf.Cos((n * Mathf.PI) / 2.0f);
@@ -82,7 +84,7 @@ namespace UGL.Easing
                 : n == 1.0f
                 ? 1.0f
                 : n < 0.5f ? Mathf.Pow(2.0f, 20.0f * n - 10.0f) / 2.0f
-                : (2 - Mathf.Pow(2.0f, -20.0f * n + 10.0f)) / 2.0f;
+                : (2.0f - Mathf.Pow(2.0f, -20.0f * n + 10.0f)) / 2.0f;
         }
         public static class Circ
         {
@@ -99,6 +101,7 @@ namespace UGL.Easing
             const float C2 = C1 * 1.525f;
             const float C3 = C1 + 1.0f;
 
+
             public static float In(float n) => C3 * n * n * n - C1 * n * n;
             public static float Out(float n) => 1.0f + C3 * Mathf.Pow(n - 1.0f, 3.0f) + C1 * Mathf.Pow(n - 1.0f, 2.0f);
             public static float InOut(float n)
@@ -110,6 +113,7 @@ namespace UGL.Easing
         {
             const float C4 = (2.0f * Mathf.PI) / 3.0f;
             const float C5 = (2.0f * Mathf.PI) / 4.5f;
+
 
             public static float In(float n)
                 => n == 0.0f
@@ -137,6 +141,7 @@ namespace UGL.Easing
             const float N1 = 7.5625f;
             const float D1 = 2.75f;
 
+
             public static float In(float n) => 1.0f - Out(1.0f - n);
             public static float Out(float n)
             {
@@ -148,7 +153,7 @@ namespace UGL.Easing
                 {
                     return N1 * (n -= 1.5f / D1) * n + 0.75f;
                 }
-                else if (n < 2.5 / D1)
+                else if (n < 2.5f / D1)
                 {
                     return N1 * (n -= 2.25f / D1) * n + 0.9375f;
                 }
@@ -163,40 +168,53 @@ namespace UGL.Easing
                 : (1.0f + Out(2.0f * n - 1.0f)) / 2.0f;
         }
 
-        static readonly Func<float, float>[] EasingFunctions = new Func<float, float>[]
-        {
-            Sine.In,
-            Sine.Out,
-            Sine.InOut,
-            Quad.In,
-            Quad.Out,
-            Quad.InOut,
-            Cubic.In,
-            Cubic.Out,
-            Cubic.InOut,
-            Quart.In,
-            Quart.Out,
-            Quart.InOut,
-            Quint.In,
-            Quint.Out,
-            Quint.InOut,
-            Expo.In,
-            Expo.Out,
-            Expo.InOut,
-            Circ.In,
-            Circ.Out,
-            Circ.InOut,
-            Back.In,
-            Back.Out,
-            Back.InOut,
-            Elastic.In,
-            Elastic.Out,
-            Elastic.InOut,
-            Bounce.In,
-            Bounce.Out,
-            Bounce.InOut,
+        private static Func<float, float>[] EasingMethodList { get; }
+            = new Func<float, float>[]
+            {
+                Linear,
+                Sine.In,
+                Sine.Out,
+                Sine.InOut,
+                Quad.In,
+                Quad.Out,
+                Quad.InOut,
+                Cubic.In,
+                Cubic.Out,
+                Cubic.InOut,
+                Quart.In,
+                Quart.Out,
+                Quart.InOut,
+                Quint.In,
+                Quint.Out,
+                Quint.InOut,
+                Expo.In,
+                Expo.Out,
+                Expo.InOut,
+                Circ.In,
+                Circ.Out,
+                Circ.InOut,
+                Back.In,
+                Back.Out,
+                Back.InOut,
+                Elastic.In,
+                Elastic.Out,
+                Elastic.InOut,
+                Bounce.In,
+                Bounce.Out,
+                Bounce.InOut,
         };
 
+
+        /// <summary>
+        /// Applies the specified easing function to the given value.
+        /// </summary>
+        /// <typeparam name="T">The type of the easing function, which must be an enumeration.</typeparam>
+        /// <param name="easingType">The type of easing function to apply.</param>
+        /// <param name="n">The value to which the easing function is applied.</param>
+        /// <returns>A float representing the result of the easing function applied to the value.</returns>
+        public static float Ease<T>(T easingType, float n)
+            where T : struct, Enum
+            => EasingMethodList[Convert.ToInt32(easingType)].Invoke(n);
 
         /// <summary>
         /// Normalize a given value to a specified range.
@@ -204,11 +222,11 @@ namespace UGL.Easing
         /// and `x_max`. The normalized value is then scaled to the range specified by `y_start`
         /// and `y_end`.
         /// </summary>
-        /// <param name="x">x The value to be normalized.</param>
-        /// <param name="x_min">x_min The minimum value of the range to normalize `x` to.</param>
-        /// <param name="x_max">x_max The maximum value of the range to normalize `x` to.</param>
-        /// <param name="y_start">y_start The starting value of the output range.</param>
-        /// <param name="y_end">y_end The ending value of the output range.</param>
+        /// <param name="x">The value to be normalized.</param>
+        /// <param name="x_min">The minimum value of the range to normalize `x` to.</param>
+        /// <param name="x_max">The maximum value of the range to normalize `x` to.</param>
+        /// <param name="y_start">The starting value of the output range.</param>
+        /// <param name="y_end">The ending value of the output range.</param>
         /// <returns>The normalized value of `x` in the specified output range.</returns>
         public static float Normalize(float x, float x_min, float x_max, float y_start = 0.0f, float y_end = 1.0f)
         {
@@ -232,10 +250,22 @@ namespace UGL.Easing
             // Scale the normalized value to the output range.
             return (y_end * y) + y_start;
         }
+
+        /// <summary>
+        /// Applies a normalized easing function to the given value.
+        /// </summary>
+        /// <typeparam name="T">The type of the easing function, which must be an enumeration.</typeparam>
+        /// <param name="easingType">The type of easing function to apply.</param>
+        /// <param name="x">The value to be eased.</param>
+        /// <param name="x_start">The start value of the input range.</param>
+        /// <param name="x_end">The end value of the input range.</param>
+        /// <param name="start_displacement">The displacement to be applied at the start of the output range.</param>
+        /// <param name="end_displacement">The displacement to be applied at the end of the output range.</param>
+        /// <returns>A float representing the eased and normalized value.</returns>
         public static float NormalizedEasing<T>(T easingType, float x, float x_start, float x_end, int start_displacement, int end_displacement)
             where T : struct, Enum
             => Normalize(
-                EasingFunctions[Convert.ToInt32(easingType)].Invoke(Normalize(x, x_start, x_end))
+                Ease(easingType, Normalize(x, x_start, x_end))
                 , 0.0f
                 , 1.0f
                 , start_displacement
